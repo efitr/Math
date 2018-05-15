21463876c98b6f5d5ae72c28a6b9eb397840bee0
Egon Fiedler <efiedler93@gmail.com>
2018-05-14 23:50:05 -0700

    pattern

---
+++ permutations_singledigit_poweroftwo.py
@@ -0,0 +1,13 @@
+
+################################################################################
+
+    #Objective: How many posibilities of number combinations there are in 2 to the power 
+    # of its values has long has they dont result in anything more than a single digit number
+
+
+def two_tothepower_of_anything():
+#Automaticaly generates all the permutations of 
+
+
+#This must be
+    return 
