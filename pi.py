1185e019032a1bf420e99fd1be7481c584c3718c
Egon Fiedler <efiedler93@gmail.com>
2018-09-15 00:05:41 -0700

    Adding file, break on a determined number of spaces, next step what number of numerator is

---
+++ pi.py
@@ -0,0 +1,24 @@
+
+''' How to get π:  
+        π = 4( 1/1 + 1/3 + 1/5 + 1/7 + 1/9 + 1/11 ... )
+'''
+
+def calculation to get pi(numerator):
+    const = 4
+    for i 
+        #
+        const 
+    
+
+if __name__ = '__main__':
+# ### ##### ####### ######### ########### ############# ############# ############### 
+    input('Please insert a unpair number' + numerator)
+
+    if numerator % 2 not 0:
+        print("No señor No señor No señor no es asi")
+        
+    #stop at a certain number of spaces
+
+
+    #make it be a string
+
